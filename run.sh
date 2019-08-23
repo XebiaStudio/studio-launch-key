ff7b9aaaf4743d4c7e6a4569445040bd64f22003
arnestaphorsius <arne.staphorsius@gmail.com>
2019-08-23 11:40:18 +0200

    Set some variables, write a first loop to turn on a led

--- run.sh
+++ run.sh
@@ -1,24 +1,54 @@
 # !/bin/bash
 
-# export a pin if it has not been exported yet
+# export pin $1 if it has not been exported yet
 exportPin() {
 	if [ ! -e /sys/class/gpio/gpio$1 ]; then
 		echo "$1" > /sys/class/gpio/export
 	fi
 }
 
+# set the direction of pin $1 to be an input
 setInput() {
 	echo "in" > /sys/class/gpio/gpio$1/direction
 }
 
+# set the direction of pin $1 to be an output
 setOutput() {
 	echo "out" > /sys/class/gpio/gpio$1/direction
 }
 
+# set pin $1 to value $2
 setValue() {
-	echo "$2" > /sys/class/gpio/gpio$1/value
+	echo $2 > /sys/class/gpio/gpio$1/value
 }
 
-exportPin 12
-exportPin 18
-exportPin 22
+# get value from pin $1
+valueOf() {
+    echo /sys/class/gpio/gpio$1/value
+}
+
+# set the pin numbers we'll be using
+KEY_1=12
+KEY_2=13
+BTN_PRESS=21
+BTN_LED=22
+
+ON="1"
+OFF="0"
+
+exportPin $KEY_1
+exportPin $KEY_2
+exportPin $BTN_PRESS
+exportPin $BTN_LED
+
+setOutput $KEY_1
+setOutput $KEY_2
+setOutput $BTN_PRESS
+setOutput $BTN_LED
+
+while true; do
+    if [ "$(valueOf $KEY_1)" == $ON && "$(valueOf $KEY_2)" == $ON]; then
+        setValue $BTN_LED $ON
+    fi
+    sleep 1
+done
